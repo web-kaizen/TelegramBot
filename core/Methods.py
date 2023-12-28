968f9ba53a45f9ea00f1eb9be3392515305c53ef
WooDMaNbtw <nikita.profatilov@gmail.com>
2023-12-28 22:13:29 +0400

    Добавлена обработка html, а также изменен request_setter

--- core/Methods.py
+++ core/Methods.py
@@ -1,6 +1,6 @@
+from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from .settings import APP_ID, THIRD_PARTY_APP_URL
 
 
@@ -8,17 +8,19 @@ class Methods:
 
     def request_setter(self, request):
         self.set_method(self.get_method())
-        self.set_url(f'{self._BASE_URL}{self._APP_ID}{self.get_path()}')
+        self.set_url(f'{self._THIRD_PARTY_APP_URL}{APP_ID}{self.get_path()}')
         self.set_headers(dict(request.headers))
 
         if self.get_method() == "GET":
-            self.set_parameters(request.query_params.dict())
+            self.set_parameters(dict(request.query_params))
         else:
             self.set_parameters(request.data)
 
     def get(self, request):
         self.request_setter(request)
         response, headers, status_code = self.send()
+        if type(response) == str:
+            return HttpResponse(status=status_code, content=response, headers=headers)
         return Response(status=status_code, data=response, headers=headers)
 
     def post(self, request):
