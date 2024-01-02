1d9353af14f36de8c599bf5723ee3bf759fbd033
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-01-02 21:29:25 +0400

    Добавлено приложение telegram_bot в INSTALLED APPS

--- core/settings.py
+++ core/settings.py
@@ -67,11 +67,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     "rest_framework",
     'rest_framework_swagger',
     'drf_yasg',
+
     "core",
     "services",
+    "telegram_bot"
 ]
 
 MIDDLEWARE = [
