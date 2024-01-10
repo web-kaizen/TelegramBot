ac79b45890598d8d4022dfeac822292c0ec0e2bc
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-01-10 20:47:05 +0400

    Исправлен HTTP DELETE метод для сообщений

--- services/MessageList.py
+++ services/MessageList.py
@@ -10,10 +10,17 @@ class MessageList(Route, APIView):
     def __init__(self):
         super().__init__()
         self.dialogue_id: Any = None
+        self.__request_headers: dict = None
 
     def dispatch(self, request: Request, **kwargs) -> HttpResponse:
         self.dialogue_id = kwargs.get("dialogue_id")
         return super().dispatch(request=request)
+
+    def request_setter(self, request):
+        self.__request_headers = dict(request.headers)
+        self.__request_headers["Content-Type"] = "application/json"
+        request.headers = self.__request_headers
+        super().request_setter(request)
 
     def get_method(self) -> str:
         return self.request.method
