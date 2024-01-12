67c91c824745d2b03d856d3884a3a334b3108fba
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-01-13 01:55:46 +0400

    fixed BotDetail.py

--- services/BotDetail.py
+++ services/BotDetail.py
@@ -1,21 +1,11 @@
-from typing import Any
-from django.http import HttpResponse
-from requests import Response, Request
 from core.Route import Route
 from rest_framework.views import APIView
 
 
 class BotDetail(Route, APIView):
-    def __init__(self):
-        super().__init__()
-        self.bot_id: Any = None
-
-    def dispatch(self, request: Request, **kwargs) -> HttpResponse:
-        self.bot_id = kwargs.get("bot_id")
-        return super().dispatch(request)
 
     def get_method(self) -> str:
         return "GET"
 
     def get_path(self) -> str:
-        return f"/bots/{self.bot_id}"
+        return f"/bots/{self._bot_id}"
