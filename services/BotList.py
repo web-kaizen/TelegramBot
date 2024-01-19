684541f8ff9cfe01c6ee93e4978bd6da3f83e18d
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-01-20 01:42:16 +0400

    Переопределен метод send с кэшированием ботов

--- services/BotList.py
+++ services/BotList.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from core.Route import Route
 from services.CustomRoute import CustomRoute
+from django.core.cache import cache
+from core.settings import CACHE_DEFAULT_TTL
 
 
 class BotList(CustomRoute, APIView):
@@ -10,3 +12,22 @@ class BotList(CustomRoute, APIView):
 
     def get_path(self) -> str:
         return "/bots"
+
+    def send(self) -> tuple:
+        body = cache.get("body")
+        headers = cache.get("headers")
+        status_code = cache.get("status_code")
+
+        if not body or not headers or not status_code:
+            response_body, response_headers, response_status_code = super().send()
+            cache.set_many(data={
+                "body": response_body,
+                "headers": response_headers,
+                "status_code": response_status_code
+            }, timeout=CACHE_DEFAULT_TTL)
+
+            return response_body, response_headers, response_status_code
+        else:
+            return body, headers, status_code
+
+
