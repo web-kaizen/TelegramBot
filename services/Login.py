cbaa1701be48a6402c8eaa22b6d417143c7f2ab1
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-01-22 15:55:16 +0400

    Оставил наследование от Route

--- services/Login.py
+++ services/Login.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
+from core.Route import Route
 from services.CustomRoute import CustomRoute
 
 
-class Login(CustomRoute, APIView):
+class Login(Route, APIView):
 
     def get_method(self) -> str:
         return "POST"
