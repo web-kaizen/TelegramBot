369c8ba0197c5762a26925920adcc87ecd2fdc04
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-01-30 20:59:13 +0400

    Добавлена переменная записи кэша

--- services/BotList.py
+++ services/BotList.py
@@ -3,6 +3,9 @@ from services.CustomRoute import CustomRoute
 
 
 class BotList(CustomRoute, APIView):
+    def __init__(self):
+        self.use_cache = True
+        super().__init__(use_cache=self.use_cache)
 
     def get_method(self) -> str:
         return "GET"
