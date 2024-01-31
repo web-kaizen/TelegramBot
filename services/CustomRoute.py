e851ae935e9ee7c254bb194a78535bb13c244539
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-01-31 23:20:02 +0400

    Измене json-ответ CustomRoute

--- services/CustomRoute.py
+++ services/CustomRoute.py
@@ -5,9 +5,9 @@ class CustomRoute(Route):
     def set_response(self, response: dict | None, headers: dict | None, status_code=None, ) -> None:
         if response:
             if 'result' in response:
-                super().set_response(response['result'], status_code)
-                response['result'] = super().get_response()
+                super().set_response(response['result'], headers, status_code)
+                response = super().get_response()
             if 'error' in response:
-                super().set_response(response['error'], status_code)
-                response['error'] = super().get_response()
+                super().set_response(response['error'], headers, status_code)
+                response = super().get_response()
         super().set_response(response, headers, status_code)
