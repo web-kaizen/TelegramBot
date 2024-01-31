32d8c40d1a275adda963380d7e4c917719105504
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-01-31 23:23:50 +0400

    Добавлен views.py

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -5,6 +5,7 @@ from aiogram.types import Message, CallbackQuery, InlineKeyboardButton
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from django.core.cache import cache
+import requests
 
 ''' django setup '''
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
@@ -19,23 +20,44 @@ router = Router()
 
 @router.message(Command("start"))
 async def start(clb: CallbackQuery):
-    email, password = f"{clb.from_user.id}@telegram.org", str(round(datetime.now().timestamp()))
-    user = User.objects.get_or_create(
-        email=email,
-        defaults={
-            "user_id": clb.from_user.id,
-            "email": email,
-            "password": generate_password_hash(password)
-        })[0]
+    cache_key = f"telegram_bot_{clb.from_user.id}"
+    cached_user = cache.get(key=cache_key)
 
-    cache_key = f"telegram_bot_{user.email}"
-    authorized = cache.get(key=cache_key)
+    if not cached_user:
+        email, password = f"{clb.from_user.id}@telegram.org", str(round(datetime.now().timestamp()))
+        user = User.objects.filter(email=email).first()
+        if not user:
+            user = User.objects.create(user_id=clb.from_user.id, email=email, password=password)
+            register = Register.Register(data={"email": user.email, "password": user.password}, need_execute_local=True)
 
-    if not authorized:
-        Register.Register(data={"email": user.email, "password": user.password}, need_execute_local=True)
-        login = Login.Login(data={"email": user.email, "password": user.password}, need_execute_local=True).get_response()
-        expires_in, token = login["result"]["expires_in"], login["result"]["access_token"]
+            if register._status_code == 200:
+                await clb.answer("Successful registration")
+            elif register._status_code == 404:
+                await clb.answer("No data was found, please try again later")
+            elif register._status_code == 409:
+                await clb.answer("User already exists")
+            elif register._status_code == 500:
+                await clb.answer("Server error, please try again later")
 
-        cache.set(key=cache_key, value={"email": user.email, "token": token, "expires_in": expires_in}, timeout=expires_in)
+        login = Login.Login(data={"email": user.email, "password": user.password}, need_execute_local=True)
+        login_response = login.get_response()
+        if login._status_code == 200:
+            expires_in = login_response.get("expires_in", 60)
+            token = login_response.get("access_token", None)
+            if not token:
+                await clb.answer("Token was not provided...")
+                raise requests.exceptions.ProxyError("Invalid token")
 
-    await main_menu(clb)
+            await clb.answer(text=f"{email}, {expires_in}, {token}, {password}")  # будет вызываться main_menu
+            cache.set(key=cache_key, value={"email": user.email, "token": token}, timeout=expires_in)
+
+        elif login._status_code == 401:
+            await clb.answer("Invalid email or password")
+        elif login._status_code == 404:
+            await clb.answer("No data was found, please try again later")
+        elif login._status_code == 500:
+            await clb.answer("Server error, please try again later")
+    else:
+        email, token = cached_user.values()  # будет вызываться main_menu
+        await clb.answer(text=f"Email: {email}\n Token: {token}")
+
