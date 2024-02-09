d7414c5f4bda00b47802effdf06155048c7c54b9
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-02-09 20:42:32 +0400

    Изменена структура start, добавлена функция log_user

--- telegram_bot/tg_services/start.py
+++ telegram_bot/tg_services/start.py
@@ -1,4 +1,6 @@
 import os, django, requests
+from typing import Any
+
 from aiogram.filters import Command
 from aiogram.types import CallbackQuery
 from datetime import datetime
@@ -8,8 +10,24 @@ from telegram_bot.run import router
 from services import Register, Login
 from telegram_bot.models import User
 
-# async def log_user() -> None:
 
+async def log_user(clb: CallbackQuery, user: User, login_status_options: dict) -> Any:
+    login = Login.Login(data={"email": user.email, "password": user.password}, need_execute_local=True)
+    login_response = login.get_response()
+
+    log_status_res = login_status_options[login._status_code]
+    if type(log_status_res) is str:
+        return await clb.answer(text=log_status_res)
+    elif log_status_res is object:
+        expires_in = login_response.get("expires_in", 60)
+        token = login_response.get("access_token", None)
+
+        if not token:
+            await clb.answer("Token was not provided...")
+            raise requests.exceptions.ProxyError("Invalid token")
+
+        cache.set(key=f"telegram_bot_{clb.from_user.id}", value={"email": user.email, "token": token},
+                  timeout=expires_in)
 
 
 @router.message(Command("start"))
@@ -34,22 +52,7 @@ async def start(clb: CallbackQuery):
     if user:
         cached_user = cache.get(key=f"telegram_bot_{clb.from_user.id}")
         if not cached_user:
-            login = Login.Login(data={"email": user.email, "password": user.password}, need_execute_local=True)
-            login_response = login.get_response()
-
-            log_status_res = login_status_options[login._status_code]
-            if type(log_status_res) is str:
-                return await clb.answer(log_status_res)
-            elif log_status_res is object:
-                expires_in = login_response.get("expires_in", 60)
-                token = login_response.get("access_token", None)
-
-                if not token:
-                    await clb.answer("Token was not provided...")
-                    raise requests.exceptions.ProxyError("Invalid token")
-
-                cache.set(key=f"telegram_bot_{clb.from_user.id}", value={"email": user.email, "token": token},
-                          timeout=expires_in)
+            await log_user(clb=clb, user=user, login_status_options=login_status_options)
 
     elif not user:
         user = User.objects.create(user_id=clb.from_user.id, email=email, password=password)
@@ -59,56 +62,8 @@ async def start(clb: CallbackQuery):
         if type(reg_status_res) is str:
             return await clb.answer(text=reg_status_res)
         elif reg_status_res is Login.Login:
-            login = Login.Login(data={"email": user.email, "password": user.password}, need_execute_local=True)
-            login_response = login.get_response()
-
-            log_status_res = login_status_options[login._status_code]
-            if type(log_status_res) is str:
-                return await clb.answer(log_status_res)
-            elif log_status_res is object:
-                expires_in = login_response.get("expires_in", 60)
-                token = login_response.get("access_token", None)
-
-                if not token:
-                    await clb.answer("Token was not provided...")
-                    raise requests.exceptions.ProxyError("Invalid token")
-
-                cache.set(key=f"telegram_bot_{clb.from_user.id}", value={"email": user.email, "token": token}, timeout=expires_in)
+            await log_user(clb=clb, user=user, login_status_options=login_status_options)
 
     return await main_menu(clb)
 
-    #
-    # elif not user:
-    #     user = User.objects.create(user_id=clb.from_user.id, email=email, password=password)
-    #     register = Register.Register(data={"email": user.email, "password": user.password}, need_execute_local=True)
-    #
-    #     if register._status_code == 200:
-    #         login = Login.Login(data={"email": user.email, "password": user.password}, need_execute_local=True)
-    #         login_response = login.get_response()
-    #
-    #         if login._status_code == 200:
-    #             expires_in = login_response.get("expires_in", 60)
-    #             token = login_response.get("access_token", None)
-    #             if not token:
-    #                 await clb.answer("Token was not provided...")
-    #                 raise requests.exceptions.ProxyError("Invalid token")
-    #
-    #             cache.set(key=f"telegram_bot_{clb.from_user.id}", value={"email": user.email, "token": token}, timeout=expires_in)
-    #
-    #         elif login._status_code == 401:
-    #             await clb.answer("Invalid email or password")
-    #         elif login._status_code == 404:
-    #             await clb.answer("No data was found, please try again later")
-    #         elif login._status_code == 500:
-    #             await clb.answer("Server error, please try again later")
-    #
-    #     elif register._status_code == 404:
-    #         await clb.answer("No data was found, please try again later")
-    #     elif register._status_code == 409:
-    #         await clb.answer("User already exists")
-    #     elif register._status_code == 500:
-    #         await clb.answer("Server error, please try again later")
-    #
-    # return await main_menu(clb)
-    #
 
