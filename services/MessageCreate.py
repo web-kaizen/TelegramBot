ad37ac002c13fe7869d177aa5320cea232bf9560
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-02-15 19:33:59 +0400

    Изменил наследование, добавил dialogue_id как обязательный параметр

--- services/MessageCreate.py
+++ services/MessageCreate.py
@@ -1,17 +1,18 @@
 from rest_framework.views import APIView
 from core.Route import Route
+from services.CustomRoute import CustomRoute
 
 
-class MessageCreate(Route, APIView):
-    def __init__(self, data: dict = None, headers: dict = {}, token=None, need_execute_local=False):
+class MessageCreate(CustomRoute, APIView):
+    def __init__(self, dialogue_id, data: dict = None, headers: dict = {}, token=None, need_execute_local=False):
         self.data: dict = data
         self.headers: dict = headers
         self.headers["Authorization"] = f"Bearer {token}"
+        self._dialogue_id = dialogue_id
         super().__init__(need_execute_local)
 
     def get_path(self) -> str:
         return f"/dialogues/{self._dialogue_id}/messages"
 
     def get_method(self) -> str:
-
         return 'POST'
