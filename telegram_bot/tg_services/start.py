3df40d0953a1a64cf1b3673d9351d38c75644b3e
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-02-15 19:45:22 +0400

    Исправленны недочеты первой версии функции start

--- telegram_bot/tg_services/start.py
+++ telegram_bot/tg_services/start.py
@@ -1,23 +1,26 @@
 import os, django, requests
 from typing import Any
+
+from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import CallbackQuery
 from datetime import datetime
 from django.core.cache import cache
 from .main_menu import main_menu
-from .assets import router
-
-from services import Register, Login, BotList, BotDetail, DialogueList
+from services import Register, Login
 from telegram_bot.models import User
 
+router = Router()
 
-async def log_user(clb: CallbackQuery, user: User, login_status_options: dict) -> Any:
+
+async def log_user(clb: CallbackQuery, user: User, login_status_options: dict, tg_user_id: int) -> Any:
     login = Login.Login(data={"email": user.email, "password": user.password}, need_execute_local=True)
     login_response = login.get_response()
 
     log_status_res = login_status_options[login._status_code]
     if type(log_status_res) is str:
         return await clb.answer(text=log_status_res)
+
     elif log_status_res is object:
         expires_in = login_response.get("expires_in", 60)
         token = login_response.get("access_token", None)
@@ -26,12 +29,13 @@ async def log_user(clb: CallbackQuery, user: User, login_status_options: dict) -
             await clb.answer("Token was not provided...")
             raise requests.exceptions.ProxyError("Invalid token")
 
-        cache.set(key=f"telegram_bot_{clb.from_user.id}", value={"email": user.email, "token": token},
+        cache.set(key=f"telegram_bot_{tg_user_id}", value={"email": user.email, "token": token},
                   timeout=expires_in)
 
 
 @router.message(Command("start"))
 async def start(clb: CallbackQuery):
+    tg_user_id = clb.from_user.id
     register_status_options = {
         200: Login.Login,
         404: "No data was found, please try again later",
@@ -45,26 +49,24 @@ async def start(clb: CallbackQuery):
         404: "No data was found, please try again later",
         500: "Server error, please try again later"
     }
-
-    email, password = f"{clb.from_user.id}@telegram.org", str(round(datetime.now().timestamp()))
+    email, password = f"{tg_user_id}@telegram.org", str(round(datetime.now().timestamp()))
     user = User.objects.filter(email=email).first()
 
     if user:
-        cached_user = cache.get(key=f"telegram_bot_{clb.from_user.id}")
+        cached_user = cache.get(key=f"telegram_bot_{tg_user_id}")
         if not cached_user:
-            await log_user(clb=clb, user=user, login_status_options=login_status_options)
-
+            await log_user(clb=clb, user=user, login_status_options=login_status_options, tg_user_id=tg_user_id)
     elif not user:
-        user = User.objects.create(user_id=clb.from_user.id, email=email, password=password)
+        user = User.objects.create(user_id=tg_user_id, email=email, password=password)
         register = Register.Register(data={"email": user.email, "password": user.password}, need_execute_local=True)
         register_response = register.get_response()
         reg_status_res = register_status_options[register._status_code]
         if type(reg_status_res) is str:
-            return await clb.answer(text=reg_status_res)
+            await clb.answer(text=reg_status_res)
         elif reg_status_res is Login.Login:
             user.user_core_id = register_response.get("user_id", None)
             user.save()
-            await log_user(clb=clb, user=user, login_status_options=login_status_options)
+            await log_user(clb=clb, user=user, login_status_options=login_status_options, tg_user_id=tg_user_id)
 
     return await main_menu(clb)
 
