ed816dcb3c93cc65dd8a29a0493f52cb98317542
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-03-04 20:14:39 +0400

    sys.path added

--- telegram_bot/run.py
+++ telegram_bot/run.py
@@ -1,5 +1,8 @@
 import os, django, asyncio, logging
 from aiogram import Dispatcher, Bot
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 ''' django setup '''
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
