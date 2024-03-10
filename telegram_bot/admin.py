4439c5746669c8aca20c1cc14d0e768d94ef3970
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-03-10 16:52:54 +0400

    admin changed, fields displayed

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -11,6 +11,6 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(UserStatistic)
 class UserStatisticAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'type', 'name', 'max_value', 'min_value']
+    list_display = ['id', 'user', 'type', 'name', 'current_dialogues', 'max_dialogues']
     list_filter = ("id", "user", "type", "name")
     search_fields = ('user', "type", 'name')
